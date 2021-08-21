40500820d056c1c5f425eb2ef4b5a8ff6d017c25
Tamim <farman.arefin1@outlook.com>
2021-08-21 12:28:28 +0600

    Added first js

---
+++ js/app.js
@@ -0,0 +1,10 @@
+
+const memoryButton1 = document.getElementById('memory-button-1');
+const memoryButton2 = document.getElementById('memory-button-2');
+
+
+const storageButton1 = document.getElementById('storage-button-1');
+const storageButton2 = document.getElementById('storage-button-2');
+
+const deliveryCharge1 = document.getElementById('delivery-charge-1');
+const deliveryCharge2 = document.getElementById('delivery-charge-2');
