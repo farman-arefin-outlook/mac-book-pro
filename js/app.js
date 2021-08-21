e3bbb093c7a58b788071916f6967ab84518091ca
Tamim <farman.arefin1@outlook.com>
2021-08-21 12:47:09 +0600

    Update function

--- js/app.js
+++ js/app.js
@@ -5,6 +5,20 @@ const memoryButton2 = document.getElementById('memory-button-2');
 
 const storageButton1 = document.getElementById('storage-button-1');
 const storageButton2 = document.getElementById('storage-button-2');
+const storageButton3 = document.getElementById('storage-button-3');
 
 const deliveryCharge1 = document.getElementById('delivery-charge-1');
 const deliveryCharge2 = document.getElementById('delivery-charge-2');
+
+let totalPrice = 1299;
+
+function updateMemoryCost(extraMemoryCost) {
+    debugger;
+    memoryButton1.addEventListener('click', function () {
+        const extraMemoryCostUpdate = parseInt(extraMemoryCost) + 50;
+        extraMemoryCost.innerText = extraMemoryCostUpdate;
+        totalPrice = totalPrice + extraMemoryCostUpdate;
+    });
+}
+const extraMemoryCost = document.getElementById('extra-memory-cost');
+updateMemoryCost(extraMemoryCost);
